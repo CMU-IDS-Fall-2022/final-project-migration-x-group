6cd2b4e1ada1bc0980d09f3100e5d0cab476fc68
DreamNengNeng <75749274+DreamNengNeng@users.noreply.github.com>
2022-12-02 11:10:31 -0500

    Update migration_app.py

--- migration_app.py
+++ migration_app.py
@@ -164,7 +164,8 @@ lines = rate_by_state_race.mark_line().encode(
     size=alt.condition(~highlight, alt.value(1), alt.value(3))
 )
 st.write(points + lines)
-st.subheader("Race Black has the highest average migration rate across U.S.")
+with st.beta_expander('\U+1F609 Insights'):
+    st.subheader("Race Black has the highest average migration rate across U.S.")
 
 ##VIZ 3
 st.header("Popular Migration Routes")
