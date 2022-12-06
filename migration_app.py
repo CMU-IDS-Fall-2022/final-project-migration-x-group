e330a69ff084f24445a4d996dd53086e928a52f9
DreamNengNeng <75749274+DreamNengNeng@users.noreply.github.com>
2022-12-06 15:59:24 -0500

    Update migration_app.py

--- migration_app.py
+++ migration_app.py
@@ -227,6 +227,7 @@ choropleth_grade.geojson.add_child(
 
 st.write("#### Educational Score in 2018 for all States in the United States")
 left = st_folium(education_map, width=600, height=400)
+right = st.write('Massachussetts and New York have the highest gradings in 2008') 
 st.write("data source: https://www.edweek.org/policy-politics/grading-the-states/2008/01")
 
 # Correlation 
@@ -236,7 +237,8 @@ df_higher_education = pd.read_csv("data/Higher_Edu_RatioByState.csv")
 correlation = df_migration['inbound_migration_rate'].corr(df_higher_education['higher_edu_ratio'])
 c1 = str(round(correlation,3))
 st.subheader('corrleation score:' + c1)
-st.subheader("\U0001F348 There is a positive relationship between higher education ratio and inbound migration rate") 
+st.subheader("\U0001F348 There is a positive relationship between higher education ratio and inbound migration rate.") 
+
 
 ################################################ Inbound Migration ###################################################
 st.header("Inbound Migration Pattern Analysis")
